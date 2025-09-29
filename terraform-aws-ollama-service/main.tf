10aa39e7a42b69b6072ed2c288194e2e803b0d65
jose <jose.chavez@bitmaker.la>
2025-09-29 18:22:46 -0500

    Add Initial infrustructure

--- terraform-aws-ollama-service/main.tf
+++ terraform-aws-ollama-service/main.tf
@@ -16,3 +16,94 @@ output "ami_dl_name" {
   description = "deep learning ami name"
 }
 
+module "vpc" {
+  source = "./modules/vpc"
+
+  name            = "ollama-server-1"
+  vpc_cidr        = "10.0.0.0/16"
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.11.0/24", "10.0.12.0/24"]
+  azs             = ["us-east-1a", "us-east-1b"]
+}
+
+resource "aws_security_group" "ollama_sg" {
+  name        = "ollama-sg"
+  description = "Allow SSH and Ollama API"
+  vpc_id      = module.vpc.vpc_id
+  tags = {
+    Name = "Ollama-sg"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_access_ollama" {
+  description       = "Give access to ollama"
+  cidr_ipv4         = "179.6.164.239/32" # Change this with your IP
+  cidr_ipv6         = null
+  ip_protocol       = "-1"
+  security_group_id = aws_security_group.ollama_sg.id
+  from_port         = null
+  to_port           = null
+}
+
+resource "aws_vpc_security_group_egress_rule" "ollama_outbound_ipv4" {
+  security_group_id = aws_security_group.ollama_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = null
+  ip_protocol       = "-1"
+  to_port           = null
+}
+
+resource "aws_vpc_security_group_egress_rule" "ollama_outbound_ipv6" {
+  security_group_id = aws_security_group.ollama_sg.id
+  cidr_ipv6         = "::/0"
+  from_port         = null
+  ip_protocol       = "-1"
+  to_port           = null
+}
+
+resource "aws_instance" "ollama_server" {
+  ami           = data.aws_ami.ubuntu_dl_gpu.id
+  instance_type = "g6.xlarge"
+  subnet_id     = module.vpc.public_subnets[0]
+  key_name      = aws_key_pair.generated.key_name
+
+  root_block_device {
+    delete_on_termination = true
+    encrypted             = false
+    tags = {
+      Name = "Ollama volume"
+    }
+    volume_type = "gp3"
+    volume_size = 80 # size in GB
+  }
+
+  connection {
+    user        = "ubuntu"
+    private_key = tls_private_key.generated.private_key_pem
+    host        = self.public_ip
+  }
+
+  provisioner "local-exec" {
+    command = "chmod 600 ${local_file.private_key_pem.filename}"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo chmod 1777 /tmp",
+      "curl -fsSL https://ollama.com/install.sh | sh",
+      "ollama pull gpt-oss:20b",
+      "sudo mkdir -p /etc/systemd/system/ollama.service.d",
+      "echo '[Service]' | sudo tee /etc/systemd/system/ollama.service.d/override.conf",
+      "echo 'Environment=\"OLLAMA_HOST=0.0.0.0:11434\"' | sudo tee -a /etc/systemd/system/ollama.service.d/override.conf",
+      "echo 'Environment=\"OPENAI_API_KEY=ollama\"' | sudo tee -a /etc/systemd/system/ollama.service.d/override.conf",
+      "sudo systemctl daemon-reload",
+      "sudo systemctl restart ollama.service",
+    ]
+  }
+
+  vpc_security_group_ids = [aws_security_group.ollama_sg.id]
+
+  tags = {
+    Name = "Ollama server"
+  }
+}
