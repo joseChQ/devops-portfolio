cd773406be48a781b4ab3822636d1495dfc08de0
jose <jose.chavez@bitmaker.la>
2025-09-29 18:20:53 -0500

    Add provider for Keypair keypair generation

--- terraform-aws-ollama-service/terraform.tf
+++ terraform-aws-ollama-service/terraform.tf
@@ -4,6 +4,14 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 6.14"
     }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.1"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.1.0"
+    }
   }
   required_version = ">= 1.9"
 }
